b4b1e89005c21b50c1273432d31df0e9f069c4a1
Kaushal Shah <kaushalshah1307@gmail.com>
2015-03-14 18:24:22 -0400

    Hello World Program

---
+++ Hello World/src/introduction.java
@@ -0,0 +1,10 @@
+
+public class introduction {
+
+	public static void main(String[] args) {
+		//Print out the Hello World code appended with your name to introduce yourself
+		String Name = "Kaushal Shah";
+		System.out.println("Hello World! I am " +Name);
+	}
+
+}
